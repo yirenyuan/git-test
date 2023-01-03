d36fa36fa3253fc976cd5930f02c6207f2d3236e
Yiry <yirenyuan1997@163.com>
2023-01-03 15:40:52 +0800

    hotfix first commit

--- src/main/java/com/arguigu/git/GitTest.java
+++ src/main/java/com/arguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hi,Git");
         System.out.println("Hi,Git");
+        System.out.println("Hi,Git hot-fix");
 
     }
 }
