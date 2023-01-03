16ee48dbcf9c6717f10547fded69442b3e913e5a
Yiry <yirenyuan1997@163.com>
2023-01-03 15:31:01 +0800

    thrid commit

--- src/main/java/com/arguigu/git/GitTest.java
+++ src/main/java/com/arguigu/git/GitTest.java
@@ -3,5 +3,7 @@ package com.arguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hi,Git");
+        System.out.println("Hi,Git");
+
     }
 }
